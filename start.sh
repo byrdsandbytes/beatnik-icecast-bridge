414dbd84123955bc4add12ee4b2de914cc72e251
idrimi <gerber@byrds.ch>
2026-02-24 17:23:23 +0100

    fix sample rate mismatch

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 snapclient -h $SNAPSERVER_IP --logsink stderr --player file | \
-ffmpeg -f s16le -ar 48000 -ac 2 -i pipe:0 \
+ffmpeg -f s16le -ar 44100 -ac 2 -i pipe:0 \
 -codec:a libmp3lame -b:a 128k -content_type audio/mpeg \
 -f mp3 $ICECAST_URL

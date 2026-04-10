719e9859a2579e17089bcf85111ff19ad6da3ccd
idrimi <gerber@byrds.ch>
2026-04-10 15:33:56 +0200

    revert to 16 and volume limiter

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 snapclient -h $SNAPSERVER_IP --logsink stderr --player file --mixer none | \
-ffmpeg -f s32le -ar 48000 -ac 2 -i pipe:0 \
--af "alimiter=limit=0.99" \
+ffmpeg -f s16le -ar 48000 -ac 2 -i pipe:0 \
+-af "volume=0.85,alimiter=limit=0.95" \
 -codec:a libmp3lame -b:a 128k -content_type audio/mpeg \
 -f mp3 $ICECAST_URL
